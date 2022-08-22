8e21711dd4e7159aa2a920a818777b5a109ebf92
luis <lgonzalesu@ulasalle.edu.pe>
2022-08-22 11:29:11 -0500

    vistas actualizadas

---
+++ src/components/DashboardVue.vue
@@ -0,0 +1,68 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+    >
+    
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Menu
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            <!-- texto -->
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          @click="$router.push({ path: item.route })"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    
+      <!--  -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Ugel-BuResol 1.0</v-toolbar-title>
+    </v-app-bar>
+    
+    <v-main>
+      
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data: () => ({ 
+      items: [
+          { title: 'Búsqueda', icon: 'mdi-view-dashboard',route:'/busquedas'},
+          { title: 'Cuenta', icon: 'mdi-account' },
+          { title: 'Soporte', icon: 'mdi-nut' },
+        ],
+      right: null,
+      drawer: null
+    }),
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -4,7 +4,32 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+import VueRouter from 'vue-router'
+Vue.use(VueRouter);
+
+import HelloWorld from './components/HelloWorld.vue'
+import BusquedaVue from './components/BusquedaVue.vue'
+
+const router = new VueRouter({
+  mode : 'history',
+  base: __dirname,
+  routes:[
+  {
+    path:'/logins',
+    name:'login',
+    component:HelloWorld
+  },
+  {
+    path:'/busquedas',
+    name:'busqueda',
+    component:BusquedaVue
+  }
+]
+})
+
+
 new Vue({
   vuetify,
+  router,
   render: h => h(App)
 }).$mount('#app')
